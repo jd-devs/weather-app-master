78bc88c659970bef16f934f007ca14e5ba5b47e2
msdario12 <mansilladario94@gmail.com>
2023-06-03 20:05:27 -0300

    Initial setup of react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import '../dist/output.css';
+
+function App() {
+	return (
+		<>
+			<div className='bg-gray-900 h-screen'>
+				<h1 className='text-blue-50 text-4xl'>Hola mundo</h1>
+			</div>
+		</>
+	);
+}
+
+export default App;
